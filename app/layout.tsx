2042730720dbb85d8e95c95ce4c615ff38ca9e5e
pr4dhan <pradhan.cook@gmail.com>
2025-04-02 00:05:22 +0530

    next js update

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,11 +81,11 @@ export default function RootLayout({
         }}
       />
 
-      <html lang="en">
+      <html>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[linear-gradient(to_right,rgba(0,0,0,0.025)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.025)_1px,transparent_1px)] bg-[size:14px_25px] dark:bg-[linear-gradient(to_right,rgba(255,255,255,0.025)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.025)_1px,transparent_1px)] dark:bg-[size:14px_25px]`}
         >
-          <ThemeProvider attribute="class" defaultTheme="light">
+          <ThemeProvider attribute="class" defaultTheme="system">
             <Header />
             {children}
             <TrackUserDevice />
