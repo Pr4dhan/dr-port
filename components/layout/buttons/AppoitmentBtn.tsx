5d3617f23f45393cec06ac0ba7f44dd9a620e1bc
pr4dhan <pradhan.cook@gmail.com>
2025-04-20 17:41:00 +0530

    appoinment button resize

--- components/layout/buttons/AppoitmentBtn.tsx
+++ components/layout/buttons/AppoitmentBtn.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const AppointmentBtn: React.FC = () => {
   return (
     <Link href="/appointment">
-      <button className="px-3 py-1 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-gray-500">
+      <button className="px-2 py-2 text-xs sm:px-3 sm:py-1 sm:text-base rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-gray-500">
         Book Appointment
       </button>
     </Link>
